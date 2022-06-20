30b6e9048b64ca9f5055941e491fbfad288cc5f4
manoj <56719591+bhartimanoj@users.noreply.github.com>
2022-06-20 13:05:25 +0530

    make it better

--- _controllers/student.controller.js
+++ _controllers/student.controller.js
@@ -3,8 +3,8 @@ const ObjectID = require('mongodb').ObjectId;
 const student = require('../_models/student.model');
 
 exports.retrieveRecords = (req, res) => {
-    student.find({}).then((students) => {
-        res.status(200).send({ err: false, msg: "Successfully retrived sub agenices.", data: students });
+    student.find({}).sort({ _id: -1 }).then((students) => {
+        res.status(200).send({ err: false, msg: "Successfully retrived Student List.", data: students });
     }).catch((err) => {
         res.status(500).send({ err: true, msg: err });
     });
@@ -28,7 +28,7 @@ exports.retrieveDetails = (req, res) => {
 }
 
 exports.updateRecord = (req, res) => {
-    student.findByIdAndUpdate(ObjectID(req.params.id), req.body).then((student) => {
+    student.updateOne({ _id: ObjectID(req.params.id) }, req.body).then((student) => {
         res.status(200).send({ err: false, msg: "Successfully updated student.", data: student });
     }).catch((err) => {
         res.status(500).send({ err: true, msg: err });
@@ -36,8 +36,8 @@ exports.updateRecord = (req, res) => {
 }
 
 exports.deleteRecord = (req, res) => {
-    student.findByIdAndDelete(ObjectID(req.params.id)).then((student) => {
-        if (student) {
+    student.deleteOne({ _id: ObjectID(req.params.id) }).then((student) => {
+        if (student.deletedCount == 1) {
             res.status(200).send({ err: false, msg: "Successfully deleted student." });
         } else {
             res.status(404).send({ err: true, msg: "Student not found." });
